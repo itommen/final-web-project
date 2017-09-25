d253552d6c23087587656c84e90add428864c32a
Dana Eliyahu <danaeliyahu@cs.colman.ac.il>
2017-09-25 19:53:02 +0300

    remove unused classes

--- Recipes/Recipes/Controllers/ManageController.cs
+++ Recipes/Recipes/Controllers/ManageController.cs
@@ -306,15 +306,7 @@ namespace Recipes.Controllers
                 CurrentLogins = userLogins,
                 OtherLogins = otherLogins
             });
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult LinkLogin(string provider)
-        {
-            // Request a redirect to the external login provider to link a login for the current user
-            return new AccountController.ChallengeResult(provider, Url.Action("LinkLoginCallback", "Manage"), User.Identity.GetUserId());
-        }
+        }        
 
         public async Task<ActionResult> LinkLoginCallback()
         {
